1a29cdc386f89ca840381dcf6a7cbc9eaf554d9d
Sharifullah Faqery <Shariffaqery@gmail.com>
2021-12-14 21:21:55 -0500

    First commit of Jenkins

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Osman Bey is the greatest ever");
+            Thread.sleep(2000);
+
+        }
+    }
+
+}

b992b227725666b49813a9a603afdac02391b5d5
Sharifullah Faqery <Shariffaqery@gmail.com>
2021-12-14 21:43:33 -0500

    second Commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Osman Bey is the greatest ever");
             Thread.sleep(2000);
 
